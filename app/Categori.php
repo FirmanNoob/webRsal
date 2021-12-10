5ac2adea04bce2458c18562837c29f6f037783f3
FirmanNoob <syah91177@gmail.com>
2021-12-10 08:10:45 +0700

    Second Commit

---
+++ app/Categori.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Categori extends Model
+{
+    protected $table = 'table_categoris';
+    protected $guarded = ['id'];
+
+    public function setSlugAttribute($value)
+    {
+        $this->attributes['slug'] = Str::slug($value, '-');
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    public function Artikel()
+    {
+        return $this->hasMany('\App\Artikel', 'categoris_id', 'id');
+    }
+}
